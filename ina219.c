13889a47064173332179b5002d8d80f99811dbe8
Montmasson Emmanuel <emmanuel.montmasson@insa-rennes.fr>
2022-06-13 11:36:22 +0200

    Remove HAL_Delay() in INA219_Reset(), add I2C status trace

--- ina219.c
+++ ina219.c
@@ -4,12 +4,14 @@
  * Based on the github repository : https://github.com/komuch/PSM_INA219_STM32
  */
 
-#include "INA219.h"
+#include "ina219.h"
 
 /* === Include here the HAL header corresponding to your board === */
-// #include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal.h"
 
 #include <math.h>
+
+HAL_StatusTypeDef INA219_I2C_Status;
 
 
 uint8_t INA219_Init(INA219_t *ina219, I2C_HandleTypeDef *i2c, uint8_t Address, double maxCurrent)
@@ -17,13 +19,13 @@ uint8_t INA219_Init(INA219_t *ina219, I2C_HandleTypeDef *i2c, uint8_t Address, d
 	ina219->ina219_i2c = i2c;
 	ina219->Address = Address;
 
-	uint8_t ina219_isReady = HAL_I2C_IsDeviceReady(i2c, (Address << 1), 3, 2);
+	HAL_StatusTypeDef ina219_isReady = HAL_I2C_IsDeviceReady(i2c, (Address << 1), 3, 2);
 
 	if(ina219_isReady == HAL_OK)
 	{
 		INA219_Reset(ina219);	// Reset the INA219
 
-		ina219->current_LSB = maxCurrent/pow(2, 15);
+		ina219->current_LSB = maxCurrent/pow(2.0, 15.0);
 
 		double resistor = ((double)SHUNT_RESISTOR)/1000.0;
 		// Using datasheet formula
@@ -90,7 +92,6 @@ double INA219_ReadPower(INA219_t * ina219)
 void INA219_Reset(INA219_t *ina219)
 {
 	Write16(ina219, INA219_REG_CONFIG, INA219_CONFIG_RESET);
-	HAL_Delay(1);	// Give some time to the INA to update its registers
 }
 
 uint16_t INA219_getConfig(INA219_t *ina219)
@@ -124,7 +125,7 @@ uint16_t Read16(INA219_t *ina219, uint8_t Register)
 {
 	uint8_t Value[2];
 
-	HAL_I2C_Mem_Read(ina219->ina219_i2c, (ina219->Address << 1), Register, 1, Value, 2, 1000);
+	INA219_I2C_Status = HAL_I2C_Mem_Read(ina219->ina219_i2c, (ina219->Address << 1), Register, 1, Value, 2, HAL_MAX_DELAY);
 
 	return ((Value[0] << 8) | Value[1]);
 }
@@ -135,5 +136,5 @@ void Write16(INA219_t *ina219, uint8_t Register, uint16_t Value)
 	uint8_t addr[2];
 	addr[0] = (Value >> 8) & 0xff;  // MSByte
 	addr[1] = (Value >> 0) & 0xff; // LSByte
-	HAL_I2C_Mem_Write(ina219->ina219_i2c, (ina219->Address << 1), Register, 1, (uint8_t*)addr, 2, 1000);
+	INA219_I2C_Status = HAL_I2C_Mem_Write(ina219->ina219_i2c, (ina219->Address << 1), Register, 1, (uint8_t*)addr, 2, HAL_MAX_DELAY);
 }
